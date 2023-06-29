203c7a692bb82783603d89e62a06bcd28c078be8
StudentLin <346454850@qq.com>
2023-06-29 17:22:49 +0800

    update settings

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -93,19 +93,5 @@ export default defineUserConfig({
         },
       ],
     },
-    // commentConfig: {
-    //   type: 'valie',
-    //   // options 与 1.x 的 valineConfig 配置一致
-    //   options: {
-    //     // appId: 'xxx',
-    //     // appKey: 'xxx',
-    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     // verify: true, // 验证码服务
-    //     // notify: true,
-    //     // recordIP: true,
-    //     // hideComments: true // 隐藏评论
-    //   },
-    // },
   }),
-  // debug: true,
 });
