62e2f27d3cb0e16f8bbf24545638733fef9df540
StudentLin <346454850@qq.com>
2023-06-29 16:32:23 +0800

    add dist to .gitignore

--- .vuepress/dist/assets/api.html-204ccdf6.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-acdd9360","path":"/docs/theme-reco/api.html","title":"api","lang":"en-US","frontmatter":{"title":"api","date":"2020/05/29"},"headers":[],"git":{"createdTime":1688023945000,"updatedTime":1688023945000,"contributors":[{"name":"StudentLin","email":"346454850@qq.com","commits":1}]},"filePathRelative":"docs/theme-reco/api.md"}');export{e as data};
--- .vuepress/dist/assets/theme.html-1bda9918.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-65959fc1","path":"/docs/theme-reco/theme.html","title":"theme","lang":"en-US","frontmatter":{"title":"theme","date":"2020/05/27"},"headers":[],"git":{"createdTime":1688023945000,"updatedTime":1688023945000,"contributors":[{"name":"StudentLin","email":"346454850@qq.com","commits":1}]},"filePathRelative":"docs/theme-reco/theme.md"}');export{e as data};
